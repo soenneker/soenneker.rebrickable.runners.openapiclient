66fc534103bb3178d8e23d2da77c4fff4579329d
Jake Soenneker <jake@soenneker.com>
2025-07-26 13:25:20 -0500

    Initial commit

---
+++ src/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace Soenneker.Rebrickable.Runners.OpenApiClient;
+
+public static class Constants
+{
+    public const string Library = "Soenneker.Rebrickable.OpenApiClient";
+}
---
+++ test/Soenneker.Rebrickable.Runners.OpenApiClient.Tests/RebrickableOpenApiClientRunnerTests.cs
@@ -0,0 +1,19 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Rebrickable.Runners.OpenApiClient.Tests;
+
+[Collection("Collection")]
+public sealed class RebrickableOpenApiClientRunnerTests : FixturedUnitTest
+{
+    public RebrickableOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
